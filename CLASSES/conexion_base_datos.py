74fef2c2d29ad0fe2118965246352a65234249ba
bayron.ortiz <bayrondanilo92@gmail.com>
2017-12-14 10:11:42 -0500

    cambiado ruta

--- CLASSES/conexion_base_datos.py
+++ CLASSES/conexion_base_datos.py
@@ -7,11 +7,10 @@ from CLASSES.gmail_api import GmailAPI
 import datetime as dt
 
 # Ruta a Base de Datos
-#PATH_BASE_DATOS = "http://superiorcore:71ZRUSThCdIOO5MYikj1@superiorcore.sb02.stations.graphenedb.com:24789/db/data/"
-#PATH_BASE_DATOS = "http://neo4j:neo1234@192.168.1.1:7474/db/data"  # Acceso Remoto
-PATH_BASE_DATOS = "localhost:7474"  # Acceso local
-USUARIO = "neo4j"           # Acceso local
-PASS = "sistemas"           # Acceso local
+PATH_BASE_DATOS = "http://neo4j:sistemas@172.17.235.217:7474/db/data"  # Acceso Remoto
+#PATH_BASE_DATOS = "localhost:7474"  # Acceso local
+#USUARIO = "neo4j"           # Acceso local
+#PASS = "sistemas"           # Acceso local
 #PATH_BASE_DATOS = "192.168.1.1:7474"
 #PASS = "admin_neo"
 #PASS = "neo1234"
@@ -27,9 +26,9 @@ class ConexionBaseDatos():
     """
     def __init__(self):
         super(ConexionBaseDatos, self).__init__()
-        authenticate(PATH_BASE_DATOS, USUARIO, PASS)    # Acceso local
-        self.graph = Graph()                   # Acceso Local
-        #self.graph = Graph(PATH_BASE_DATOS)       # Acceso Remoto
+        #authenticate(PATH_BASE_DATOS, USUARIO, PASS)    # Acceso local
+        #self.graph = Graph()                   # Acceso Local
+        self.graph = Graph(PATH_BASE_DATOS)       # Acceso Remoto
 
     def consultar_grupo(self, grupo):
         """
